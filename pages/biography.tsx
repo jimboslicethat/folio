20156ec5d2efde10169159806f5e5d537f6f7512
James Walsh <jamesandersonwalsh@gmail.com>
2020-10-29 21:37:45 -0600

    Add typewriter animation to bio

--- pages/biography.tsx
+++ pages/biography.tsx
@@ -4,11 +4,9 @@ import styles from './biography.module.css'
 
 export default function Biography() {
   return (
-    <blockquote className={styles.quote} aria-label="About me">
-      I'm a proud geek, and life long learner. I'm excited about everything related to full stack
-      development, computer science, & technology in general. From the apps that make me more
-      productive, to the IoT devices that power my house; I consistenly find new and compelling
-      reasons to write "Hello World" one more time.
-    </blockquote>
+    <section className={styles.quote} aria-label="About me">
+      <span className={styles.firstLine}>I'm a proud geek, and life long learner.</span>
+      <span className={styles.secondLine}>Who can't seem to stop printing "Hello World".</span>
+    </section>
   )
 }
