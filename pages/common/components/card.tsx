32d8c809266e8322aeb5ac6d59a4aefbe8d38b6c
James Walsh <jamesandersonwalsh@gmail.com>
2020-10-29 22:46:57 -0600

    Use material design icons

--- pages/common/components/card.tsx
+++ pages/common/components/card.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
 
 import styles from './card.module.css'
 
@@ -7,17 +6,17 @@ type CardProps = {
   title: string
   href: string
   description: string
-  imageUrl?: string
+  icon?: any
 }
 
-export default function Card({ href, title, description, imageUrl }: CardProps) {
+export default function Card({ href, title, description, icon }: CardProps) {
   return (
     <section className={styles.card}>
-      {imageUrl ? (
-        <Image src={imageUrl} alt={`Logo for ${title}`} width="36rem" height="36rem" />
-      ) : null}
       <a target="_blank" href={href}>
-        <h3 className={styles.title}>{title} &rarr;</h3>
+        <div className={styles.cardHeader}>
+          {icon}
+          <h3 className={styles.title}>{title}</h3>
+        </div>
         <p className={styles.description}>{description}</p>
       </a>
     </section>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
 import Head from 'next/head'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import MailIcon from '@material-ui/icons/Mail'
+import CreateIcon from '@material-ui/icons/Create'
 
 import styles from './index.module.css'
 import Card from './common/components/card'
@@ -12,6 +16,13 @@ export default function Home() {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
@@ -22,26 +33,28 @@ export default function Home() {
         <div className={styles.grid}>
           <Card
             title="Github"
-            imageUrl="/github-mark-light.png"
+            icon={<GitHubIcon fontSize="large" />}
             href="https://github.com/jimboslicethat"
             description="Find out more about how I collaborate & my pet projects"
           />
 
           <Card
             title="LinkedIn"
-            imageUrl="/linkedin-logo.png"
+            icon={<LinkedInIcon fontSize="large" />}
             href="https://www.linkedin.com/in/jamesandersonwalsh/"
             description="My preferred way to share my resume with your team"
           />
 
           <Card
             title="Blog Posts"
+            icon={<CreateIcon fontSize="large" />}
             href="https://dev.to/jimboslicethat/"
             description="Check out my posts on my favorite blog platform, dev.to"
           />
 
           <Card
             title="Contact Me"
+            icon={<MailIcon fontSize="large" />}
             href="mailto:jamesandersonwalsh@gmail.com"
             description="I'm always an email or direct message away 👍"
           />
