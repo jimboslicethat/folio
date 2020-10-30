79d877d5658de6c69195908786ced5cd5c972670
James Walsh <jamesandersonwalsh@gmail.com>
2020-10-29 22:53:02 -0600

    Tweak footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,7 +66,8 @@ export default function Home() {
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer">
-          Deployed with <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Built using Next.js. Special thanks to{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
