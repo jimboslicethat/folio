16f1e9e59d1455e9a7a57837cf645394c8955df5
James Walsh <jamesandersonwalsh@gmail.com>
2020-10-29 21:42:38 -0600

    Shorten animation width

--- pages/biography.tsx
+++ pages/biography.tsx
@@ -5,8 +5,8 @@ import styles from './biography.module.css'
 export default function Biography() {
   return (
     <section className={styles.quote} aria-label="About me">
-      <span className={styles.firstLine}>I'm a proud geek, and life long learner.</span>
-      <span className={styles.secondLine}>Who can't seem to stop printing "Hello World".</span>
+      <span className={styles.firstLine}>I'm a proud geek, & life long learner</span>
+      <span className={styles.secondLine}>Who can't stop printing "Hello World"</span>
     </section>
   )
 }
