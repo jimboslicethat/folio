08fb462ae206f7664e54c32b7b06511ee17a4dfa
James Walsh <jamesandersonwalsh@gmail.com>
2020-10-29 23:34:57 -0600

    Create common color variables

---
+++ pages/common/colors.ts
@@ -0,0 +1,11 @@
+const brightBlue = '#5cc8ff'
+const darkGray = '#161215'
+const lightGray = '#2d252a'
+const white = '#fffafb'
+
+export default {
+  brightBlue,
+  darkGray,
+  lightGray,
+  white
+}
