3c47ee20c63f6eb3f41da27748d9f9950bed7daa
James Walsh <jamesandersonwalsh@gmail.com>
2020-10-30 16:10:05 -0600

    Add some blogs to the blog post page

--- pages/blog-posts.tsx
+++ pages/blog-posts.tsx
@@ -2,11 +2,28 @@ import React from 'react'
 import Footer from './common/components/footer'
 
 import NavBar from './common/components/navbar'
+import styles from '../styles/blog-posts.module.css'
 
 export default function BlogPosts(): React.ReactElement {
   return (
     <>
       <NavBar />
+      <main className={styles.container}>
+        <article>
+          <iframe
+            className={styles.blogPost}
+            src="https://dev.to/jimboslicethat/from-bootcamp-to-college-43on"
+            title="From Bootcamp to College"
+          />
+        </article>
+        <article>
+          <iframe
+            className={styles.blogPost}
+            src="https://dev.to/jimboslicethat/the-mac-guy-guide-to-windows-development-4m5c"
+            title="The Mac Guy Guide to Windows Development"
+          />
+        </article>
+      </main>
       <Footer />
     </>
   )
