6a16c8f90fa63c43a803d8aa1e6b29a044237e88
James Walsh <jamesandersonwalsh@gmail.com>
2021-01-13 19:39:21 -0700

    refactor(blog.tsx): refactor blog page to retrieve posts from dev.to

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,3 +1,6 @@
+import CommentIcon from '@material-ui/icons/Comment'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import LabelIcon from '@material-ui/icons/Label'
 import React from 'react'
 import useSWR from 'swr'
 
@@ -5,16 +8,45 @@ import Footer from '../components/footer'
 import NavBar from '../components/navbar'
 import styles from '../styles/pages/blog.module.css'
 
+const iconStyles = { color: '#0088CC', marginRight: 4 }
+
 export default function BlogPosts(): React.ReactElement {
-  const { data } = useSWR('/api/posts', () => fetch('/api/posts').then(_ => _.json()))
-  // eslint-disable-next-line no-console
-  console.log(data)
+  const { data } = useSWR('/api/posts', () => fetch('/api/posts').then(res => res.json()))
+
+  if (!data) return <div>loading...</div>
 
   return (
     <>
       <NavBar />
       <main className={styles.container}>
-        <h2>Under construction!</h2>
+        <h1>Blog Posts</h1>
+        <li className={styles.list}>
+          {data.map(post => (
+            <ul key={post.id} className={styles.listItem}>
+              <h2 className={styles.title}>{post.title}</h2>
+              <div>{post.description}</div>
+              <div className={styles.metadata}>
+                <span className={styles.metadataContent}>
+                  <FavoriteIcon style={{ ...iconStyles, color: '#FF6B6B' }} />
+                  {post.positive_reactions_count}
+                </span>
+                <span className={styles.metadataContent}>
+                  <CommentIcon style={iconStyles} />
+                  {post.comments_count}
+                </span>
+                <span className={styles.metadataContent}>
+                  <LabelIcon style={iconStyles} />
+                  <span className={styles.emphasized}>{post.tag_list.map(tag => `#${tag} `)}</span>
+                </span>
+                <span className={styles.metadataContent}>
+                  <span className={styles.emphasized}>
+                    {new Date(post.published_at).toDateString()}
+                  </span>
+                </span>
+              </div>
+            </ul>
+          ))}
+        </li>
       </main>
       <Footer />
     </>
